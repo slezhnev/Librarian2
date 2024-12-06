bcb36a9302a3aca8e3410ab5dd17552f7ca51538
Sergey Lezhnev <sergey.lezhnev@netcracker.com>
2024-12-06 19:03:59 +0300

    Some additional Angular improvements

--- src/main/webui/src/app/app.component.ts
+++ src/main/webui/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { BookInfo } from './bookinfo.component'
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BookInfo],
+  imports: [RouterOutlet, BookInfo, ReactiveFormsModule, MatRadioModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   title = 'librarian2';
+
+  searchType: string = "Authors";
+
+  searchForm: FormGroup = new FormGroup({
+    searchEdit: new FormControl('', Validators.required)
+  })
+
+  handleSearch() {
+    alert(this.searchForm.value.searchEdit + " " + this.searchType)
+  }
+
 }
