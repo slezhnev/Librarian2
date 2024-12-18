6ca8e16cc31366f1270f618a254098d09116faff
Sergey Lezhnev <sergey.lezhnev@netcracker.com>
2024-12-18 17:14:44 +0300

    Book info are moved to Material. Some events implementation for author and series switch from book info

--- src/main/webui/src/app/authorlink.component.ts
+++ src/main/webui/src/app/authorlink.component.ts
@@ -1,19 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Book, Author} from "./models"
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
 	selector: 'authorlink',
 	templateUrl: './authorlink.component.html',
+	imports: [MatButtonModule],
 })
 
 export class AuthorLink {
 	
 	@Input() author : Author|null = null;
+	@Output() switchToAuthorEvent = new EventEmitter<Author>();
 
 	switchToAuthor() {
-		// Do nothing at the moment - but we should send event to search part aftewards
 		if (this.author) {
-			console.info("Author " + this.author?.authorId + " selected - should be loaded after")
+			console.debug(" Trying to switch to author " + this.author?.authorId)
+			this.switchToAuthorEvent.emit(this.author);
 		} else {
 			console.error("No author - it is quite strange")
 		}
--- src/main/webui/src/app/bookinfo.component.ts
+++ src/main/webui/src/app/bookinfo.component.ts
@@ -1,24 +1,38 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, Output, EventEmitter } from '@angular/core';
 import { DownloadService } from "./download.service"
-import { Book } from "./models"
+import { Book, Author } from "./models"
 import { AuthorLink } from './authorlink.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
 	selector: 'bookinfo',
-	imports: [AuthorLink],
+	imports: [AuthorLink, MatButtonModule, MatCardModule],
 	templateUrl: './bookinfo.component.html',
 })
 
 export class BookInfo {
-	
+
 	ds = inject(DownloadService)
 
-	@Input()
-	book: Book|null = null;
-	
+	@Input() book: Book | null = null;
+	@Output() switchToAuthorEvent = new EventEmitter<Author>();
+	@Output() switchToSerieEvent = new EventEmitter<string>();
+
 	downloadBook() {
 		if (this.book && this.book.bookId && this.book.libraryId) {
 			this.ds.downloadBook(this.book.bookId, this.book.libraryId);
-		}	
+		}
+	}
+
+	switchToAuthor(author: Author) {
+		// Send event higher
+		this.switchToAuthorEvent.emit(author);
+	}
+
+	switchToSerie() {
+		if (this.book) {
+			this.switchToSerieEvent.emit(this.book.serieName);
+		}
 	}
 }
