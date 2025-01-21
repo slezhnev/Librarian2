8e38b20374ec059deac8db1fbcb14f835c6d93b6
Sergey Lezhnev <s.lezhnev@gmail.com>
2025-01-21 17:12:16 +0300

    Worked Docker build + publication to registry

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
-call ./mvnw compile install || exit /b
-"%JAVA_HOME%/bin/java.exe" -Dlibrary.storagePath.override="library.storagePath.override.properties" -jar target/quarkus-app/quarkus-run.jar 
+call ./mvnw package || exit /b
+podman build -f src/main/docker/Dockerfile.jvm -t quarkus/librarian2-jvm .
+podman push --tls-verify=false quarkus/librarian2-jvm docker://192.168.8.4:17001/quarkus/librarian2-jvm:1.0
